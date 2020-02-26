dea4062c5f8183fbe5810f9849f2566e17f1cc59
nscar <nsotocar@gmail.com>
2020-02-26 18:25:06 -0300

    Updates

---
+++ precache-manifest.e8e0b046a356b4b76ca2ff4290ec6a24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c195cb687e327b63db12797f4e8da0b",
+    "url": "/ovel/index.html"
+  },
+  {
+    "revision": "2f9b12648f959d23d20b",
+    "url": "/ovel/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4091d30fb9ca19cac582",
+    "url": "/ovel/static/js/2.bdee2603.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ovel/static/js/2.bdee2603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f9b12648f959d23d20b",
+    "url": "/ovel/static/js/main.a024e21a.chunk.js"
+  },
+  {
+    "revision": "8e50a6e061e7ac51c3f0",
+    "url": "/ovel/static/js/runtime-main.b7f314ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ovel/static/media/logo.5d5d9eef.svg"
+  }
+]);
